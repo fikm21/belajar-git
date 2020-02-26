25a8eceb46b18c4283761890af379315acc22e6a
fikm21 <taufikm211@gmail.com>
2020-02-26 08:56:20 +0700

    modified

--- hapus.php
+++ hapus.php
@@ -1,2 +1,16 @@
-<?php
-echo "ini file kedua untuk mengapus";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hapus</title>
+</head>
+
+<body>
+    <?php
+    echo "ini file kedua untuk mengapus";
+    ?>
+</body>
+
+</html>
--- tambah.php
+++ tambah.php
@@ -1,2 +1,16 @@
-<?php
-echo "ini file untuk menambahkan satu file";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah</title>
+</head>
+
+<body>
+    <?php
+    echo "ini file untuk menambahkan satu file";
+    ?>
+</body>
+
+</html>
